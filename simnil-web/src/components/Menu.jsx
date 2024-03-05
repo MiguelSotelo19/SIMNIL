9bd15663a602f66b14e44a9223f13201807bf5c2
Migue <20223TN113@utez.edu.mx>
2024-03-04 19:41:45 -0600

    Avance de la app web

--- simnil-web/src/components/Menu.jsx
+++ simnil-web/src/components/Menu.jsx
@@ -1,9 +1,14 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
 import logo_simnil from '../assets/LogoSimnil.png';
 import home from '../assets/casa.png';
 import user from '../assets/usuario.png';
 import grafica from '../assets/tendencia.png';
 import users from '../assets/grupo.png';
 import pozo from '../assets/pozo.png';
+
+import '../css/body.css';
 
 export const Menu = () => {
     return(
@@ -17,17 +22,39 @@ export const Menu = () => {
     
 }
 
-export const SideMenu = () => {
+export const SideMenu = ({selected}) => {
+    const [selectedItem, setSelectedItem] = useState(selected);
+
     return(
         <>
         <div id="izq">
             <h2 style={{textAlign: 'center', width: '100%'}}>Menú</h2>
-                <li><img src={grafica} id='icono' /> Estadísticas</li>
-                <li><img src={users} id='icono' /> Usuarios</li>
-                <li><img src={pozo} id='icono' /> Pozos</li>
-                <li><img src={home} id='icono' /> Comunidades</li>
-                <li><img src={user} id='icono' /> Perfil</li>
+
+                <li className={selectedItem == 1 ? "hover": ""} onClick={() => {
+                    window.location ='/Estadisticas';
+                }}><img src={grafica} id='icono' /> Estadísticas</li>
+
+                <li className={selectedItem == 2 ? "hover": ""} onClick={() => {
+                    window.location = '/Usuarios';
+                }}><img src={users} id='icono' /> Usuarios</li>
+
+                <li className={selectedItem == 3 ? "hover": ""} onClick={() => {
+                    window.location = '/Pozos';
+                }}><img src={pozo} id='icono' /> Pozos</li>
+
+                <li className={selectedItem == 4 ? "hover": ""} onClick={() => {
+                    window.location = '/Comunidades';
+                }}><img src={home} id='icono' /> Comunidades</li>
+
+                <li className={selectedItem == 5 ? "hover": ""} onClick={() => {
+                    window.location = '/Perfil';
+                }}><img src={user} id='icono' /> Perfil</li>
         </div>
         </>
     );
+}
+
+
+SideMenu.propTypes = {
+    selected: PropTypes.number.isRequired
 }
--- simnil-web/src/screens/Estadisticas.jsx
+++ simnil-web/src/screens/Estadisticas.jsx
@@ -1,4 +1,3 @@
-
 import { Menu, SideMenu } from '../components/Menu'
 import { Header } from '../components/Header_'
 import Flex from '@react-css/flex'
@@ -7,23 +6,49 @@ import Flex from '@react-css/flex'
 import '../css/main.css'
 import '../css/header.css'
 import '../css/body.css'
-
+ 
 export const Estadisticas = () => {
     return(
         <>
         <Flex row id='container'>
         <div id='elements'>
           <Menu />
-          <Header />
+          <Header usuario='Administrador' />
         </div>
 
         <div id='main'>
-          <SideMenu />
+          <SideMenu selected={1} />
           <div id="der">
             <div id="der-main">
               <h1>Consulta de Información</h1>
               <hr />
-              
+
+              <div id="opciones">
+                <div id="btn-pozo">
+                  <p>Pozo</p>
+                  <select>
+                    <option>HOLA</option>
+                  </select>
+                </div>
+
+                <div id="btn-inicio">
+                  <p>Fecha Inicio</p>
+                  <select>
+                    <option>HOLA</option>
+                  </select>
+                </div>
+
+                <div id="btn-fin">
+                <p>Fecha Fin</p>
+                  <select>
+                    <option>HOLA</option>
+                  </select>
+                </div>
+              </div>
+
+              <h1 style={{marginTop: '3%'}}>Histograma</h1>
+              <hr />
+
             </div>
           </div>
         </div>
