27354d9d21158a6b1bbc1b207f6887ec553a1ee1
LuzGarcia1620 <20223tn155@utez.edu.mx>
2024-03-13 14:31:29 -0600

    graficas v1

--- simnil-movil/screen/histogramas.jsx
+++ simnil-movil/screen/histogramas.jsx
@@ -1,18 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 import { Header } from '../elements/Header';
+import { Dimensions } from "react-native";
 
-Estadistica =()=>{
-    return(
-        <View style={styles.container}>
-        <Header />    
+
+const Estadistica = () => {
+  const screenWidth = Dimensions.get("window").width;
+  const data = {
+    labels: ['Satelite', 'jacarandas', 'Temixco', 'Zapata', 'xochitepec'],
+    datasets: [
+      {
+        data: [550, 210, 300, 600, 100],
+      },
+    ],
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.chartContainer}>
+        <BarChart
+          data={data}
+          width={350}
+          height={220}
+          chartConfig={{
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(255, 35, 90, ${opacity})`,
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+            borderColor: '#000000', 
+            borderWidth: 2, 
+          }}
+          bezier
+        />
       </View>
+    </View>
+  );
+};
 
-    );
-}
 export default Estadistica;
 
 const styles = StyleSheet.create({
-    container: {
-      width: '100%'
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  chartContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
