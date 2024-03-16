e5962ce54eefd6a25ae5382477d7c4408b1fc128
Migue <20223TN113@utez.edu.mx>
2024-03-15 23:54:38 -0600

    Savepoint para histograma v3 Fecha Inicio Funcional

--- simnil-web/src/screens/Estadisticas.jsx
+++ simnil-web/src/screens/Estadisticas.jsx
@@ -1,91 +1,76 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Menu, SideMenu } from '../components/Menu';
 import { Header } from '../components/Header_';
 import Flex from '@react-css/flex';
 import axios from 'axios';
-
-//CSS
-import '../css/main.css';
-import '../css/header.css';
-import '../css/body.css';
-
 import Histograma from '../components/Histograma';
 
 export const Estadisticas = () => {
-  const urlPozos='http://localhost:8080/api/simnil/pozos/';
+  const urlPozos = 'http://localhost:8080/api/simnil/pozos/';
   const [pozos, setPozos] = useState([]);
-  const [ fechasArray, setFechas ] = useState([]);
-  const [ fechasArrayFin, setFechasFin ] = useState([]);
-  const [ fechaInicio, setFechaInicio ] = useState(''); 
-  const [ fechaFin, setFechaFin] = useState('');
- 
-  useEffect( () => {
-    getFechas();
+  const [fechasArray, setFechas] = useState([]);
+  const [fechasArrayFin, setFechasFin] = useState([]);
+  const [fechaInicio, setFechaInicio] = useState('');
+  const [fechaFin, setFechaFin] = useState('');
+
+  useEffect(() => {
+    const getPozos = async () => {
+      try {
+        const respuesta = await axios.get(urlPozos);
+        setPozos(respuesta.data.data);
+        getFechas(respuesta.data.data);
+      } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+      }
+    };
+    getPozos();
   }, []);
 
-  const getPozos = async () => {
-    try {
-      const respuesta = await axios.get(urlPozos);
-      setPozos(respuesta.data.data);
-    } catch (error) {
-      console.error('Error al obtener datos de la API:', error);
-    }
-  }
-
-  const getFechas = async () => {
-    await getPozos(); 
-
+  const getFechas = (pozosData) => {
     let fechas = [];
-    for (let i = 0; i < pozos.length; i++) {
-      const pozo = pozos[i];
-      
-      for(let j = 0; j < pozo.datosPozoBeans.length; j++){
+    for (let i = 0; i < pozosData.length; i++) {
+      const pozo = pozosData[i];
+      for (let j = 0; j < pozo.datosPozoBeans.length; j++) {
         const datos = pozo.datosPozoBeans[j];
-    
         let anio = parseInt(datos.fechaRecopilacion.substr(0, 4), 10);
         let mes = parseInt(datos.fechaRecopilacion.substr(5, 2), 10);
         let dia = parseInt(datos.fechaRecopilacion.substr(8, 2), 10);
         let hora = parseInt(datos.horaRecopilacion.substr(0, 2), 10);
         let minutos = parseInt(datos.horaRecopilacion.substr(3, 2), 10);
         let segundos = parseInt(datos.horaRecopilacion.substr(6, 2), 10);
-    
-        let fecha ={ x: new Date(anio, mes, dia, hora, minutos, segundos), y: datos.nivelAgua };
-    
+        let fecha = { x: new Date(anio, mes, dia, hora, minutos, segundos), y: datos.nivelAgua };
         fechas.push(fecha);
       }
     }
     fechas.sort((a, b) => a.x - b.x);
-    
-    for(let i=0; i<fechas.length; i++){ 
-      let fecha = (fechas[i].x+"").substr(0, 15);
-      if(i == fechas.length-1) break; 
 
-      if(fecha == ((fechas[i+1].x+"").substr(0, 15))) { 
+    for (let i = 0; i < fechas.length; i++) {
+      let fecha = (fechas[i].x + '').substr(0, 15);
+      if (i === fechas.length - 1) break;
+      if (fecha === (fechas[i + 1].x + '').substr(0, 15)) {
         fechas.splice(i, 1);
         i--;
       }
     }
+    setFechas(fechas);
+  };
 
-    console.log(fechas); 
-    setFechas(fechas); 
-  }
-
-  const asignarFechas = (inicio) => { 
+  const asignarFechas = (inicio) => {
     let arrayAux = [];
     for (let i = 0; i < fechasArray.length; i++) {
       const element = fechasArray[i].x;
-      inicio = new Date(inicio); 
+      inicio = new Date(inicio);
 
-      if(element.getTime() > inicio.getTime()){
-        arrayAux.push(fechasArray[i]); 
+      if (element.getTime() > inicio.getTime()) {
+        arrayAux.push(fechasArray[i]);
       }
     }
-    setFechasFin(arrayAux); 
-  }
+    setFechasFin(arrayAux);
+  };
 
-    return(
-        <>
-        <Flex row id='container'>
+  return (
+    <>
+      <Flex row id='container'>
         <div id='elements'>
           <Menu />
           <Header usuario='Administrador' />
@@ -103,7 +88,7 @@ export const Estadisticas = () => {
                   <p>Fecha Inicio</p>
                   <select onChange={(e) => { setFechaInicio(e.target.value); asignarFechas(e.target.value); }}>
                     <option value={null}></option>
-                    {fechasArray.map((fecha) => (
+                    {fechasArray.length > 0 && fechasArray.map((fecha) => (
                       <option key={fecha.x} value={fecha.x}>{fecha.x.toLocaleDateString()}</option>
                     ))}
                   </select>
@@ -113,14 +98,14 @@ export const Estadisticas = () => {
                   <p>Fecha Fin</p>
                   <select onChange={(e) => { setFechaFin(e.target.value); }}>
                     <option value={null}></option>
-                    {fechasArrayFin.map((fecha) => (
+                    {fechasArrayFin.length > 0 && fechasArrayFin.map((fecha) => (
                       <option key={fecha.x} value={fecha.x}>{fecha.x.toLocaleDateString()}</option>
                     ))}
                   </select> 
                 </div>  
               </div>
 
-              <h1 style={{marginTop: '3%'}}>Histograma</h1>
+              <h1 style={{ marginTop: '3%' }}>Histograma</h1>
               <hr />
 
               <Histograma fechaInicio={fechaInicio} fechaFin={fechaFin} />
@@ -129,6 +114,6 @@ export const Estadisticas = () => {
           </div>
         </div>
       </Flex>
-        </>
-    );
+    </>
+  );
 }
